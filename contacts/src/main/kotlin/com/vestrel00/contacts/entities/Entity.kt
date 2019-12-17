45498127894702ddac403402c903968fd4d85aaa
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-16 23:31:07 -0600

    Added Entity

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/Entity.kt
@@ -0,0 +1,21 @@
+package com.vestrel00.contacts.entities
+
+/**
+ * Type of all entities provided in this library.
+ */
+interface Entity {
+
+    /**
+     * The ID of this entity (row) in the table it belongs to.
+     */
+    val id: Long
+
+    /**
+     * Returns true if this entity has a valid id, which indicates that it is (or was at the time of
+     * retrieval) an existing entity in the database.
+     */
+    fun hasValidId(): Boolean = id > INVALID_ID
+
+}
+
+internal const val INVALID_ID: Long = -1L
