f56168f7530d820c15862b7aab8fca919abb95ba
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-20 00:59:44 -0600

    Added OptionsCursor

--- contacts/src/main/kotlin/com/vestrel00/contacts/entities/cursor/CursorExtensions.kt
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/cursor/CursorExtensions.kt
@@ -1,12 +1,18 @@
 package com.vestrel00.contacts.entities.cursor
 
 import android.database.Cursor
+import android.net.Uri
 import com.vestrel00.contacts.AbstractField
 import java.util.*
 
 internal fun Cursor.getString(field: AbstractField): String? {
     val index = getColumnIndex(field.columnName)
     return if (index == -1) null else getString(index)
+}
+
+internal fun Cursor.getBoolean(field: AbstractField): Boolean? {
+    val int = getInt(field)
+    return int != null && int == 1
 }
 
 internal fun Cursor.getInt(field: AbstractField): Int? {
@@ -17,6 +23,11 @@ internal fun Cursor.getInt(field: AbstractField): Int? {
 internal fun Cursor.getLong(field: AbstractField): Long? {
     val index = getColumnIndex(field.columnName)
     return if (index == -1) null else getLong(index)
+}
+
+internal fun Cursor.getUri(field: AbstractField): Uri? {
+    val uriStr = getString(field)
+    return if (uriStr != null) Uri.parse(uriStr) else null
 }
 
 internal fun Cursor.getDate(field: AbstractField): Date? {
---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/cursor/OptionsCursor.kt
@@ -0,0 +1,34 @@
+package com.vestrel00.contacts.entities.cursor
+
+import android.database.Cursor
+import android.net.Uri
+import com.vestrel00.contacts.Fields
+import com.vestrel00.contacts.entities.INVALID_ID
+import java.util.*
+
+/**
+ * Retrieves [Fields.Options] data from the given [cursor].
+ *
+ * This does not modify the [cursor] position. Moving the cursor may result in different attribute
+ * values.
+ */
+internal class OptionsCursor(private val cursor: Cursor) {
+
+    val id: Long
+        get() = cursor.getLong(Fields.Id) ?: INVALID_ID
+
+    val starred: Boolean?
+        get() = cursor.getBoolean(Fields.Options.Starred)
+
+    val timesContacted: Int?
+        get() = cursor.getInt(Fields.Options.TimesContacted)
+
+    val lastTimeContacted: Date?
+        get() = cursor.getDate(Fields.Options.LastTimeContacted)
+
+    val customRingtone: Uri?
+        get() = cursor.getUri(Fields.Options.CustomRingtone)
+
+    val sendToVoicemail: Boolean?
+        get() = cursor.getBoolean(Fields.Options.SendToVoicemail)
+}
