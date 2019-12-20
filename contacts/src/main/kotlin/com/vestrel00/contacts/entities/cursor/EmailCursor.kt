d41dbb6cd8156bb6614a4b6066e9494dabc9d6cb
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-20 00:06:05 -0600

    Added EmailCursor

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/cursor/EmailCursor.kt
@@ -0,0 +1,23 @@
+package com.vestrel00.contacts.entities.cursor
+
+import android.database.Cursor
+import com.vestrel00.contacts.Fields
+import com.vestrel00.contacts.entities.Email
+
+/**
+ * Retrieves [Fields.Email] data from the given [cursor].
+ *
+ * This does not modify the [cursor] position. Moving the cursor may result in different attribute
+ * values.
+ */
+internal class EmailCursor(cursor: Cursor) : DataCursor(cursor) {
+
+    val type: Email.Type
+        get() = Email.Type.fromValue(cursor.getInt(Fields.Email.Type))
+
+    val label: String?
+        get() = cursor.getString(Fields.Email.Label)
+
+    val address: String?
+        get() = cursor.getString(Fields.Email.Address)
+}
