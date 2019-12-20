0def03cd9652e5b4a60933282e244adad7ba5c27
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-20 00:12:25 -0600

    Added ImCursor

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/cursor/ImCursor.kt
@@ -0,0 +1,23 @@
+package com.vestrel00.contacts.entities.cursor
+
+import android.database.Cursor
+import com.vestrel00.contacts.Fields
+import com.vestrel00.contacts.entities.Im
+
+/**
+ * Retrieves [Fields.Im] data from the given [cursor].
+ *
+ * This does not modify the [cursor] position. Moving the cursor may result in different attribute
+ * values.
+ */
+internal class ImCursor(cursor: Cursor) : DataCursor(cursor) {
+
+    val protocol: Im.Protocol
+        get() = Im.Protocol.fromValue(cursor.getInt(Fields.Im.Protocol))
+
+    val customProtocol: String?
+        get() = cursor.getString(Fields.Im.CustomProtocol)
+
+    val data: String?
+        get() = cursor.getString(Fields.Im.Data)
+}
