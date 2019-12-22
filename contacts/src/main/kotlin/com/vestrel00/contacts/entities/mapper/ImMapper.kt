37ca6431442836f4d936fbc5c6b696fcc55ed50b
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-22 00:25:21 -0600

    Added ImMapper

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/mapper/ImMapper.kt
@@ -0,0 +1,19 @@
+package com.vestrel00.contacts.entities.mapper
+
+import com.vestrel00.contacts.entities.MutableIm
+import com.vestrel00.contacts.entities.cursor.ImCursor
+
+internal class ImMapper(private val imCursor: ImCursor) {
+
+    val im: MutableIm
+        get() = MutableIm(
+            id = imCursor.id,
+            rawContactId = imCursor.rawContactId,
+            contactId = imCursor.contactId,
+
+            protocol = imCursor.protocol,
+            customProtocol = imCursor.customProtocol,
+
+            data = imCursor.data
+        )
+}
