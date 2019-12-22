35d72ed614d5db5dc5af637c5ca7c361e5af69d0
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-22 00:06:08 -0600

    Added AddressMapper

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/mapper/AddressMapper.kt
@@ -0,0 +1,26 @@
+package com.vestrel00.contacts.entities.mapper
+
+import com.vestrel00.contacts.entities.MutableAddress
+import com.vestrel00.contacts.entities.cursor.AddressCursor
+
+internal class AddressMapper(private val addressCursor: AddressCursor) {
+
+    val address: MutableAddress
+        get() = MutableAddress(
+            id = addressCursor.id,
+            rawContactId = addressCursor.rawContactId,
+            contactId = addressCursor.contactId,
+
+            type = addressCursor.type,
+            label = addressCursor.label,
+
+            formattedAddress = addressCursor.formattedAddress,
+            street = addressCursor.street,
+            poBox = addressCursor.poBox,
+            neighborhood = addressCursor.neighborhood,
+            city = addressCursor.city,
+            region = addressCursor.region,
+            postcode = addressCursor.postcode,
+            country = addressCursor.country
+        )
+}
