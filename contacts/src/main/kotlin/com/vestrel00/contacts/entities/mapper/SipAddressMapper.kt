935dcf41df19ae741afb3e1803d1cf1b9b91b333
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-22 00:50:32 -0600

    Added SipAddressMapper

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/mapper/SipAddressMapper.kt
@@ -0,0 +1,16 @@
+package com.vestrel00.contacts.entities.mapper
+
+import com.vestrel00.contacts.entities.MutableSipAddress
+import com.vestrel00.contacts.entities.cursor.SipAddressCursor
+
+internal class SipAddressMapper(private val sipAddressCursor: SipAddressCursor) {
+
+    val sipAddress: MutableSipAddress
+        get() = MutableSipAddress(
+            id = sipAddressCursor.id,
+            rawContactId = sipAddressCursor.rawContactId,
+            contactId = sipAddressCursor.contactId,
+
+            sipAddress = sipAddressCursor.sipAddress
+        )
+}
