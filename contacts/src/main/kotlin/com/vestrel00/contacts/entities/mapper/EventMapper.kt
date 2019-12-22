e0716228feaaa33caefbe3798a9e0c2b6ce03805
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-22 00:20:25 -0600

    Added EventMapper

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/mapper/EventMapper.kt
@@ -0,0 +1,51 @@
+package com.vestrel00.contacts.entities.mapper
+
+import com.vestrel00.contacts.entities.MutableEvent
+import com.vestrel00.contacts.entities.cursor.EventCursor
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
+internal class EventMapper(private val eventCursor: EventCursor) {
+
+    val event: MutableEvent
+        get() = MutableEvent(
+            id = eventCursor.id,
+            rawContactId = eventCursor.rawContactId,
+            contactId = eventCursor.contactId,
+
+            type = eventCursor.type,
+            label = eventCursor.label,
+
+            date = dateFromString(eventCursor.date)
+        )
+
+    companion object {
+
+        private val DATE_FORMAT = SimpleDateFormat("yyyy-MM-dd", Locale.US)
+
+        /**
+         * The [dateStr] format is yyyy-MM-dd. E.G. 2019-08-21. This allows for greater than
+         * and less than inequality queries! Was this coincidence or did the Android team choose
+         * this format on purpose =)
+         */
+        fun dateFromString(dateStr: String?): Date? = if (dateStr != null) {
+            try {
+                DATE_FORMAT.parse(dateStr)
+            } catch (pe: ParseException) {
+                null
+            }
+        } else {
+            null
+        }
+
+        /**
+         * Outputs the [date] as a string with format [DATE_FORMAT].
+         */
+        fun dateToString(date: Date?): String? = if (date != null) {
+            DATE_FORMAT.format(date)
+        } else {
+            null
+        }
+    }
+}
