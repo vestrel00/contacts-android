ad338e1e09920aace7e72cfac9b95e7ff18afd9f
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-22 00:12:44 -0600

    Added CompanyMapper

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/mapper/CompanyMapper.kt
@@ -0,0 +1,23 @@
+package com.vestrel00.contacts.entities.mapper
+
+import com.vestrel00.contacts.entities.MutableCompany
+import com.vestrel00.contacts.entities.cursor.CompanyCursor
+
+internal class CompanyMapper(private val companyCursor: CompanyCursor) {
+
+    val company: MutableCompany
+        get() = MutableCompany(
+            id = companyCursor.id,
+            rawContactId = companyCursor.rawContactId,
+            contactId = companyCursor.contactId,
+
+            company = companyCursor.company,
+            title = companyCursor.title,
+            department = companyCursor.department,
+            jobDescription = companyCursor.jobDescription,
+            officeLocation = companyCursor.officeLocation,
+
+            symbol = companyCursor.symbol,
+            phoneticName = companyCursor.phoneticName
+        )
+}
