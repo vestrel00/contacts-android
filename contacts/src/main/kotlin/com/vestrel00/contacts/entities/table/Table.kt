42523d4bda0be1e695233ce3ba21d28e2a2fca4d
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-22 22:58:18 -0600

    Added Table

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/table/Table.kt
@@ -0,0 +1,30 @@
+package com.vestrel00.contacts.entities.table
+
+import android.net.Uri
+import android.provider.ContactsContract
+
+/**
+ * Defines all of the main [ContactsContract] tables.
+ */
+internal enum class Table(val uri: Uri) {
+
+    /**
+     * See [ContactsContract.Contacts].
+     */
+    CONTACTS(ContactsContract.Contacts.CONTENT_URI),
+
+    /**
+     * See [ContactsContract.RawContacts].
+     */
+    RAW_CONTACTS(ContactsContract.RawContacts.CONTENT_URI),
+
+    /**
+     * See [ContactsContract.Data].
+     */
+    DATA(ContactsContract.Data.CONTENT_URI),
+
+    /**
+     * See [ContactsContract.Groups].
+     */
+    GROUPS(ContactsContract.Groups.CONTENT_URI)
+}
