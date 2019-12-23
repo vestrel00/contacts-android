a5e1b1b6164ff1d013b5ad9d2305b5943647dd98
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-22 23:47:04 -0600

    Added ImOperation

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/operation/ImOperation.kt
@@ -0,0 +1,19 @@
+package com.vestrel00.contacts.entities.operation
+
+import com.vestrel00.contacts.AbstractField
+import com.vestrel00.contacts.Fields
+import com.vestrel00.contacts.entities.MimeType
+import com.vestrel00.contacts.entities.MutableIm
+
+internal class ImOperation : AbstractDataOperation<MutableIm>() {
+
+    override val mimeType = MimeType.IM
+
+    override fun setData(
+        data: MutableIm, setValue: (field: AbstractField, dataValue: Any?) -> Unit
+    ) {
+        setValue(Fields.Im.Protocol, data.protocol.value)
+        setValue(Fields.Im.CustomProtocol, data.customProtocol)
+        setValue(Fields.Im.Data, data.data)
+    }
+}
