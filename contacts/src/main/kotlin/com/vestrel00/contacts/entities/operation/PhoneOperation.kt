580d78b018c1c29b096b7a115a13918b99c2c0c9
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-23 00:02:16 -0600

    Added PhoneOperation

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/operation/PhoneOperation.kt
@@ -0,0 +1,20 @@
+package com.vestrel00.contacts.entities.operation
+
+import com.vestrel00.contacts.AbstractField
+import com.vestrel00.contacts.Fields
+import com.vestrel00.contacts.entities.MimeType
+import com.vestrel00.contacts.entities.MutablePhone
+
+internal class PhoneOperation : AbstractDataOperation<MutablePhone>() {
+
+    override val mimeType = MimeType.PHONE
+
+    override fun setData(
+        data: MutablePhone, setValue: (field: AbstractField, dataValue: Any?) -> Unit
+    ) {
+        setValue(Fields.Phone.Type, data.type.value)
+        setValue(Fields.Phone.Label, data.label)
+        setValue(Fields.Phone.Number, data.number)
+        setValue(Fields.Phone.NormalizedNumber, data.normalizedNumber)
+    }
+}
