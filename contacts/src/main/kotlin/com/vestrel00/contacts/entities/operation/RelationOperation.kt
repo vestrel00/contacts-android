3ceb77a3c39217f538740c666e7a104e678d1af4
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-23 00:04:49 -0600

    Added RelationOperation

---
+++ contacts/src/main/kotlin/com/vestrel00/contacts/entities/operation/RelationOperation.kt
@@ -0,0 +1,19 @@
+package com.vestrel00.contacts.entities.operation
+
+import com.vestrel00.contacts.AbstractField
+import com.vestrel00.contacts.Fields
+import com.vestrel00.contacts.entities.MimeType
+import com.vestrel00.contacts.entities.MutableRelation
+
+internal class RelationOperation : AbstractDataOperation<MutableRelation>() {
+
+    override val mimeType = MimeType.RELATION
+
+    override fun setData(
+        data: MutableRelation, setValue: (field: AbstractField, dataValue: Any?) -> Unit
+    ) {
+        setValue(Fields.Relation.Type, data.type.value)
+        setValue(Fields.Relation.Label, data.label)
+        setValue(Fields.Relation.Name, data.name)
+    }
+}
