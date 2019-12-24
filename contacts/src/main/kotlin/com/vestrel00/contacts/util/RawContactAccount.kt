d9689b708011d69f92b127ebffddd05085b1a6b2
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-23 22:30:15 -0600

    Added documentation to RawContactAccount

--- contacts/src/main/kotlin/com/vestrel00/contacts/util/RawContactAccount.kt
+++ contacts/src/main/kotlin/com/vestrel00/contacts/util/RawContactAccount.kt
@@ -12,8 +12,26 @@ import com.vestrel00.contacts.entities.cursor.getString
 import com.vestrel00.contacts.entities.table.Table
 import com.vestrel00.contacts.equalTo
 
+/**
+ * Queries the RawContacts table and returns the [Account] this [RawContact] belongs to.
+ *
+ * ## Permissions
+ *
+ * This requires the [ContactsPermissions.READ_PERMISSION]! This will return null if permission is
+ * not granted (or if there is no associated account).
+ */
+// [ANDROID X] @WorkerThread (not using annotation to avoid dependency on androidx.annotation)
 fun RawContact.account(context: Context): Account? = accountForRawContactWithId(id, context)
 
+/**
+ * Queries the RawContacts table and returns the [Account] this [RawContact] belongs to.
+ *
+ * ## Permissions
+ *
+ * This requires the [ContactsPermissions.READ_PERMISSION]! This will return null if permission is
+ * not granted (or if there is no associated account).
+ */
+// [ANDROID X] @WorkerThread (not using annotation to avoid dependency on androidx.annotation)
 fun MutableRawContact.account(context: Context): Account? = accountForRawContactWithId(id, context)
 
 internal fun accountForRawContactWithId(rawContactId: Long, context: Context): Account? {
