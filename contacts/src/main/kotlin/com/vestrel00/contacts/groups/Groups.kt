e8c7dfee326537602166359c8caaf030cc78fe34
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-26 23:39:00 -0600

    Added documentation to GroupsDelete and hide it from consumers

--- contacts/src/main/kotlin/com/vestrel00/contacts/groups/Groups.kt
+++ contacts/src/main/kotlin/com/vestrel00/contacts/groups/Groups.kt
@@ -3,19 +3,56 @@ package com.vestrel00.contacts.groups
 import android.content.Context
 import com.vestrel00.contacts.ContactsPermissions
 
+/**
+ * Creates a new [GroupsQuery], [GroupsInsert], and [GroupsUpdate] instances.
+ *
+ * ## Permissions
+ *
+ * - Add the "android.permission.READ_CONTACTS" to the AndroidManifest in order to [query].
+ * - Add the "android.permission.WRITE_CONTACTS" to the AndroidManifest in order to [insert] and
+ * [update].
+ *
+ * Use [permissions] convenience functions to check for required permissions.
+ *
+ * ## Deleting Groups
+ *
+ * Deleting groups, even user-created groups, is currently not supported due to some limitations
+ * imposed by the Contacts Provider. For more details, see the DEV_NOTES "Groups; Deletion" section.
+ */
 interface Groups {
 
+    /**
+     * Returns a new [GroupsQuery] instance.
+     */
     fun query(context: Context): GroupsQuery
 
+    /**
+     * Returns a new [GroupsInsert] instance.
+     */
     fun insert(context: Context): GroupsInsert
 
+    /**
+     * Returns a new [GroupsUpdate] instance.
+     */
     fun update(context: Context): GroupsUpdate
 
+    /*
+    /**
+     * Returns a new [GroupsDelete] instance.
+     */
     fun delete(context: Context): GroupsDelete
+     */
 
+    /**
+     * Returns a new [ContactsPermissions] instance, which provides functions for checking required
+     * permissions.
+     */
     fun permissions(context: Context): ContactsPermissions
 }
 
+/**
+ * Creates a new [Groups] instance.
+ */
 @Suppress("FunctionName")
 internal fun Groups(): Groups = GroupsImpl()
 
@@ -27,7 +64,7 @@ private class GroupsImpl : Groups {
 
     override fun update(context: Context) = GroupsUpdate(context)
 
-    override fun delete(context: Context): GroupsDelete = GroupsDelete(context)
+    // override fun delete(context: Context): GroupsDelete = GroupsDelete(context)
 
     override fun permissions(context: Context) = ContactsPermissions(context)
 }
--- contacts/src/main/kotlin/com/vestrel00/contacts/groups/GroupsDelete.kt
+++ contacts/src/main/kotlin/com/vestrel00/contacts/groups/GroupsDelete.kt
@@ -7,20 +7,69 @@ import com.vestrel00.contacts.ContactsPermissions
 import com.vestrel00.contacts.entities.Group
 import com.vestrel00.contacts.entities.operation.GroupOperation
 
-interface GroupsDelete {
+/**
+ * Deletes one or more groups from the groups table.
+ *
+ * FIXME? Expose this to consumers? For more details, see the DEV_NOTES "Groups; Deletion" section.
+ * Marked as internal until it can be exposed to consumers.
+ *
+ * ## Permissions
+ *
+ * The [ContactsPermissions.WRITE_PERMISSION] is assumed to have been granted already in these
+ * examples for brevity. All deletes will do nothing if the permission is not granted.
+ *
+ * ## Usage
+ *
+ * To delete the given groups;
+ *
+ * In Kotlin and Java,
+ *
+ * ```kotlin
+ * groupsDelete
+ *      .groups(groups)
+ *      .commit()
+ * ```
+ */
+internal interface GroupsDelete {
 
+    /**
+     * Adds the given [groups] to the delete queue, which will be deleted on [commit].
+     *
+     * System groups, which have [Group.readOnly] set to true, cannot be deleted and are ignored
+     * here.
+     */
     fun groups(vararg groups: Group): GroupsDelete
 
+    /**
+     * See [GroupsDelete.groups].
+     */
     fun groups(groups: Collection<Group>): GroupsDelete
 
+    /**
+     * See [GroupsDelete.groups].
+     */
     fun groups(groups: Sequence<Group>): GroupsDelete
 
+    /**
+     * Deletes the [Group]s in the queue (added via [groups]) and returns the [Result].
+     *
+     * ## Thread Safety
+     *
+     * This should be called in a background thread to avoid blocking the UI thread.
+     */
+    // [ANDROID X] @WorkerThread (not using annotation to avoid dependency on androidx.annotation)
     fun commit(): Result
 
     interface Result {
 
+        /**
+         * True if all Groups have successfully been deleted. False if even one delete failed.
+         */
         val isSuccessful: Boolean
 
+        /**
+         * True if the [group] has been successfully deleted. False otherwise.
+         */
         fun isSuccessful(group: Group): Boolean
     }
 }
