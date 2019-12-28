bcf55949099da58d9988e3c47e67c5f7b91ac10c
Vandolf Estrellado <vestrel00@gmail.com>
2019-12-27 23:16:59 -0600

    Added documentation to RawContactRefresh

--- contacts/src/main/kotlin/com/vestrel00/contacts/util/RawContactRefresh.kt
+++ contacts/src/main/kotlin/com/vestrel00/contacts/util/RawContactRefresh.kt
@@ -7,6 +7,23 @@ import com.vestrel00.contacts.entities.MutableRawContact
 import com.vestrel00.contacts.entities.RawContact
 import com.vestrel00.contacts.equalTo
 
+/**
+ * Returns the [RawContact] with all of the latest data.
+ *
+ * This is useful for getting the latest contact data after performing an update. This may return
+ * null if the [RawContact] no longer exists.
+ *
+ * ## Permissions
+ *
+ * The [com.vestrel00.contacts.ContactsPermissions.READ_PERMISSION] is required. Otherwise, null
+ * will be returned if the permission is not granted.
+ *
+ * ## Thread Safety
+ *
+ * This should be called in a background thread to avoid blocking the UI thread.
+ */
+// [ANDROID X] @WorkerThread (not using annotation to avoid dependency on androidx.annotation)
+@JvmOverloads
 fun RawContact.refresh(context: Context, cancel: () -> Boolean = { false }): RawContact? =
     Query(context)
         .where(Fields.RawContactId equalTo id)
@@ -14,6 +31,18 @@ fun RawContact.refresh(context: Context, cancel: () -> Boolean = { false }): Raw
         ?.rawContacts
         ?.firstOrNull()
 
+/**
+ * This will return [this] same instance if it does not have a valid ID, which means it is a raw
+ * contact that does not yet exist in the DB.
+ *
+ * See [RawContact.refresh].
+ *
+ * ## Thread Safety
+ *
+ * This should be called in a background thread to avoid blocking the UI thread.
+ */
+// [ANDROID X] @WorkerThread (not using annotation to avoid dependency on androidx.annotation)
+@JvmOverloads
 fun MutableRawContact.refresh(
     context: Context, cancel: () -> Boolean = { false }
 ): MutableRawContact? {
