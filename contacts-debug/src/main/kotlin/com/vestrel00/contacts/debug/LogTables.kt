3e08915243cd8e886330f5d67910c6457c6d16d2
Vandolf Estrellado <vestrel00@gmail.com>
2020-01-01 22:51:18 -0600

    Added logContactsTable function

---
+++ contacts-debug/src/main/kotlin/com/vestrel00/contacts/debug/LogTables.kt
@@ -0,0 +1,62 @@
+package com.vestrel00.contacts.debug
+
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Process
+import android.provider.ContactsContract.*
+
+fun Context.logContactsTable() {
+    if (!canLog()) {
+        log("#### Contacts table - read contacts permission not granted")
+        return
+    }
+
+    val cursor = contentResolver.query(
+        Contacts.CONTENT_URI,
+        arrayOf(
+            Contacts._ID, Contacts.DISPLAY_NAME,
+            Contacts.STARRED, Contacts.TIMES_CONTACTED, Contacts.LAST_TIME_CONTACTED,
+            Contacts.CUSTOM_RINGTONE, Contacts.SEND_TO_VOICEMAIL,
+            Contacts.PHOTO_FILE_ID, Contacts.PHOTO_URI, Contacts.PHOTO_THUMBNAIL_URI
+        ),
+        null,
+        null,
+        null
+    )
+
+    cursor ?: return
+
+    log("#### Contacts table")
+    cursor.moveToPosition(-1)
+    while (cursor.moveToNext()) {
+        val id = cursor.getString(0)
+        val displayName = cursor.getString(1)
+
+        val starred = cursor.getString(2)
+        val timesContacted = cursor.getString(3)
+        val lastTimeContacted = cursor.getString(4)
+        val customRingtone = cursor.getString(5)
+        val sendToVoicemail = cursor.getString(6)
+        val photoFileId = cursor.getString(7)
+        val photoUri = cursor.getString(8)
+        val photoThumbnailUri = cursor.getString(9)
+
+        log(
+            """
+                Contact id: $id, displayName: $displayName, starred: $starred,
+                 timesContacted: $timesContacted, lastTimeContacted: $lastTimeContacted,
+                 customRingtone: $customRingtone, sendToVoicemail: $sendToVoicemail,
+                 photoFileId: $photoFileId, photoUri: $photoUri,
+                 photoThumbnailUri: $photoThumbnailUri
+            """.trimIndent().replace("\n", "")
+        )
+    }
+
+    cursor.close()
+}
+
+// Intentionally not reusing the ContactsPermission to avoid a dependency on the contacts module.
+private fun Context.canLog(): Boolean = checkPermission(
+    Manifest.permission.READ_CONTACTS, Process.myPid(), Process.myUid()
+) == PackageManager.PERMISSION_GRANTED
