061b2864c65a41c20cb1b5f20e4db0c580fddf84
Vandolf Estrellado <vestrel00@gmail.com>
2020-01-01 22:53:49 -0600

    Added logRawContactsTable function

--- contacts-debug/src/main/kotlin/com/vestrel00/contacts/debug/LogTables.kt
+++ contacts-debug/src/main/kotlin/com/vestrel00/contacts/debug/LogTables.kt
@@ -56,6 +56,54 @@ fun Context.logContactsTable() {
     cursor.close()
 }
 
+fun Context.logRawContactsTable() {
+    if (!canLog()) {
+        log("#### RawContacts table - read contacts permission not granted")
+        return
+    }
+
+    val cursor = contentResolver.query(
+        RawContacts.CONTENT_URI,
+        arrayOf(
+            RawContacts._ID, RawContacts.CONTACT_ID,
+            RawContacts.ACCOUNT_NAME, RawContacts.ACCOUNT_TYPE,
+            RawContacts.STARRED, RawContacts.TIMES_CONTACTED, RawContacts.LAST_TIME_CONTACTED,
+            RawContacts.CUSTOM_RINGTONE, RawContacts.SEND_TO_VOICEMAIL
+        ),
+        null,
+        null,
+        null
+    )
+
+    cursor ?: return
+
+    log("#### RawContacts table")
+    cursor.moveToPosition(-1)
+    while (cursor.moveToNext()) {
+        val id = cursor.getString(0)
+        val contactId = cursor.getString(1)
+        val name = cursor.getString(2)
+        val type = cursor.getString(3)
+
+        val starred = cursor.getString(4)
+        val timesContacted = cursor.getString(5)
+        val lastTimeContacted = cursor.getString(6)
+        val customRingtone = cursor.getString(7)
+        val sendToVoicemail = cursor.getString(8)
+
+        log(
+            """
+                RawContact id: $id, contactId: $contactId, accountName: $name, accountType: $type,
+                 starred: $starred, timesContacted: $timesContacted, 
+                 lastTimeContacted: $lastTimeContacted, customRingtone: $customRingtone, 
+                 sendToVoicemail: $sendToVoicemail
+            """.trimIndent().replace("\n", "")
+        )
+    }
+
+    cursor.close()
+}
+
 // Intentionally not reusing the ContactsPermission to avoid a dependency on the contacts module.
 private fun Context.canLog(): Boolean = checkPermission(
     Manifest.permission.READ_CONTACTS, Process.myPid(), Process.myUid()
