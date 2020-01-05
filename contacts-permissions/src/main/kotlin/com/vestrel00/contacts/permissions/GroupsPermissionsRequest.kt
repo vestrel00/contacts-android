1a5c7877b9d84ce7efd39fc6d02300aa6e906252
Vandolf Estrellado <vestrel00@gmail.com>
2020-01-04 23:20:19 -0600

    Added documentation to GroupsPermissionsRequest

--- contacts-permissions/src/main/kotlin/com/vestrel00/contacts/permissions/GroupsPermissionsRequest.kt
+++ contacts-permissions/src/main/kotlin/com/vestrel00/contacts/permissions/GroupsPermissionsRequest.kt
@@ -7,6 +7,12 @@ import com.vestrel00.contacts.groups.GroupsInsert
 import com.vestrel00.contacts.groups.GroupsQuery
 import com.vestrel00.contacts.groups.GroupsUpdate
 
+/**
+ * If [ContactsPermissions.READ_PERMISSION] is not yet granted, suspends the current coroutine,
+ * requests for the permission, and then returns a new [GroupsQuery] instance.
+ *
+ * If permission is already granted, then immediately returns a new [GroupsQuery] instance.
+ */
 suspend fun Groups.queryWithPermission(activity: Activity): GroupsQuery {
     val permissions = permissions(activity)
     if (!permissions.canQuery()) {
@@ -16,6 +22,14 @@ suspend fun Groups.queryWithPermission(activity: Activity): GroupsQuery {
     return query(activity)
 }
 
+/**
+ * If [ContactsPermissions.WRITE_PERMISSION] and
+ * [com.vestrel00.contacts.accounts.AccountsPermissions.GET_ACCOUNTS_PERMISSION] are not yet
+ * granted, suspends the current coroutine, requests for the permissions, and then returns a new
+ * [GroupsInsert] instance.
+ *
+ * If permissions are already granted, then immediately returns a new [GroupsInsert] instance.
+ */
 suspend fun Groups.insertWithPermission(activity: Activity): GroupsInsert {
     val permissions = permissions(activity)
     if (!permissions.canInsertUpdateDelete()) {
@@ -25,6 +39,14 @@ suspend fun Groups.insertWithPermission(activity: Activity): GroupsInsert {
     return insert(activity)
 }
 
+/**
+ * If [ContactsPermissions.WRITE_PERMISSION] and
+ * [com.vestrel00.contacts.accounts.AccountsPermissions.GET_ACCOUNTS_PERMISSION] are not yet
+ * granted, suspends the current coroutine, requests for the permissions, and then returns a new
+ * [GroupsUpdate] instance.
+ *
+ * If permissions are already granted, then immediately returns a new [GroupsUpdate] instance.
+ */
 suspend fun Groups.updateWithPermission(activity: Activity): GroupsUpdate {
     val permissions = permissions(activity)
     if (!permissions.canInsertUpdateDelete()) {
