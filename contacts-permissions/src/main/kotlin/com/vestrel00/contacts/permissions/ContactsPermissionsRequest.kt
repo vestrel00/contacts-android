2a6c03180677d8f614bc7faf43e241931d3d18ec
Vandolf Estrellado <vestrel00@gmail.com>
2020-01-04 23:16:46 -0600

    Added documentation to ContactsPermissionsRequest

--- contacts-permissions/src/main/kotlin/com/vestrel00/contacts/permissions/ContactsPermissionsRequest.kt
+++ contacts-permissions/src/main/kotlin/com/vestrel00/contacts/permissions/ContactsPermissionsRequest.kt
@@ -3,6 +3,12 @@ package com.vestrel00.contacts.permissions
 import android.app.Activity
 import com.vestrel00.contacts.*
 
+/**
+ * If [ContactsPermissions.READ_PERMISSION] is not yet granted, suspends the current coroutine,
+ * requests for the permission, and then returns a new [Query] instance.
+ *
+ * If permission is already granted, then immediately returns a new [Query] instance.
+ */
 suspend fun Contacts.queryWithPermission(activity: Activity): Query {
     val permissions = permissions(activity)
     if (!permissions.canQuery()) {
@@ -12,6 +18,14 @@ suspend fun Contacts.queryWithPermission(activity: Activity): Query {
     return query(activity)
 }
 
+/**
+ * If [ContactsPermissions.WRITE_PERMISSION] and
+ * [com.vestrel00.contacts.accounts.AccountsPermissions.GET_ACCOUNTS_PERMISSION] are not yet
+ * granted, suspends the current coroutine, requests for the permissions, and then returns a new
+ * [Insert] instance.
+ *
+ * If permissions are already granted, then immediately returns a new [Insert] instance.
+ */
 suspend fun Contacts.insertWithPermission(activity: Activity): Insert {
     val permissions = permissions(activity)
     if (!permissions.canInsertUpdateDelete()) {
@@ -21,6 +35,14 @@ suspend fun Contacts.insertWithPermission(activity: Activity): Insert {
     return insert(activity)
 }
 
+/**
+ * If [ContactsPermissions.WRITE_PERMISSION] and
+ * [com.vestrel00.contacts.accounts.AccountsPermissions.GET_ACCOUNTS_PERMISSION] are not yet
+ * granted, suspends the current coroutine, requests for the permissions, and then returns a new
+ * [Update] instance.
+ *
+ * If permissions are already granted, then immediately returns a new [Update] instance.
+ */
 suspend fun Contacts.updateWithPermission(activity: Activity): Update {
     val permissions = permissions(activity)
     if (!permissions.canInsertUpdateDelete()) {
@@ -30,6 +52,12 @@ suspend fun Contacts.updateWithPermission(activity: Activity): Update {
     return update(activity)
 }
 
+/**
+ * If [ContactsPermissions.WRITE_PERMISSION] is not yet granted, suspends the current coroutine,
+ * requests for the permission, and then returns a new [Delete] instance.
+ *
+ * If permission is already granted, then immediately returns a new [Delete] instance.
+ */
 suspend fun Contacts.deleteWithPermission(activity: Activity): Delete {
     val permissions = permissions(activity)
     if (!permissions.canInsertUpdateDelete()) {
