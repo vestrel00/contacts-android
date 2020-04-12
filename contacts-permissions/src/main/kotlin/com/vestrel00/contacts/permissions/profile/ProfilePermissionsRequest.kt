0bbcbadd055d1ec7d2f72ea8d691a20237bd63e2
Vandolf Estrellado <vestrel00@gmail.com>
2020-04-12 00:10:05 -0500

    Added ProfilePermissionsRequest

---
+++ contacts-permissions/src/main/kotlin/com/vestrel00/contacts/permissions/profile/ProfilePermissionsRequest.kt
@@ -0,0 +1,22 @@
+package com.vestrel00.contacts.permissions.profile
+
+import android.app.Activity
+import com.vestrel00.contacts.ContactsPermissions
+import com.vestrel00.contacts.permissions.requestQueryPermission
+import com.vestrel00.contacts.profile.Profile
+import com.vestrel00.contacts.profile.ProfileQuery
+
+/**
+ * If [ContactsPermissions.READ_PERMISSION] is not yet granted, suspends the current coroutine,
+ * requests for the permission, and then returns a new [ProfileQuery] instance.
+ *
+ * If permission is already granted, then immediately returns a new [ProfileQuery] instance.
+ */
+suspend fun Profile.queryWithPermission(activity: Activity): ProfileQuery {
+    val permissions = permissions(activity)
+    if (!permissions.canQuery()) {
+        permissions.requestQueryPermission(activity)
+    }
+
+    return query(activity)
+}
