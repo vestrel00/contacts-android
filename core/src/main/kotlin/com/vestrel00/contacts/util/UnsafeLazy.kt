eefdfdb1de21b39e0fc33de711c0326a687f84b9
Vandolf Estrellado <vestrel00@gmail.com>
2020-10-25 01:25:27 +0000

    Added Profile Update commit async functions

--- core/src/main/kotlin/com/vestrel00/contacts/util/UnsafeLazy.kt
+++ core/src/main/kotlin/com/vestrel00/contacts/util/UnsafeLazy.kt
@@ -3,8 +3,7 @@ package com.vestrel00.contacts.util
 /**
  * Shorthand for `lazy(LazyThreadSafetyMode.NONE) { ... }`.
  *
- * This is used internally to reduce consumer cold startup times. Everything that is more memory or
- * CPU intensive than lazy(LazyThreadSafetyMode.NONE) { ... } should use this.
+ * This is used internally to reduce consumer cold startup times.
  */
 internal fun <T> unsafeLazy(initializer: () -> T): Lazy<T> =
     lazy(LazyThreadSafetyMode.NONE, initializer)
