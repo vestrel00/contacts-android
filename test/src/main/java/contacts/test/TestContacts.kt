473a797a11ffd093cf4f3c5da49a1894db0d0145
Vandolf Estrellado <vestrel00@gmail.com>
2021-10-30 00:21:19 +0000

    Added test module with skeleton TestContacts

---
+++ test/src/main/java/contacts/test/TestContacts.kt
@@ -0,0 +1,80 @@
+package contacts.test
+
+import android.content.Context
+import contacts.core.*
+import contacts.core.accounts.Accounts
+import contacts.core.data.Data
+import contacts.core.entities.custom.CustomDataRegistry
+import contacts.core.groups.Groups
+import contacts.core.profile.Profile
+
+@JvmOverloads
+@Suppress("FunctionName")
+fun TestContacts(
+    context: Context,
+    customDataRegistry: CustomDataRegistry = CustomDataRegistry()
+): Contacts = TestContacts(Contacts(context, customDataRegistry))
+
+object ContactsFactory {
+
+    @JvmStatic
+    @JvmOverloads
+    fun create(
+        context: Context,
+        customDataRegistry: CustomDataRegistry = CustomDataRegistry()
+    ): Contacts = TestContacts(context, customDataRegistry)
+}
+
+// Note that we cannot use "by" to delegate calls to the internal query because function calls will
+// return the internal contacts instance instead of this test instance.
+// class TestContacts(private val contacts: Contacts): Contacts by contacts
+
+/**
+ * TODO document this
+ */
+private class TestContacts(private val contacts: Contacts) : Contacts {
+
+    override fun query(): Query {
+        TODO("Not yet implemented")
+    }
+
+    override fun broadQuery(): BroadQuery {
+        TODO("Not yet implemented")
+    }
+
+    override fun insert(): Insert {
+        TODO("Not yet implemented")
+    }
+
+    override fun update(): Update {
+        TODO("Not yet implemented")
+    }
+
+    override fun delete(): Delete {
+        TODO("Not yet implemented")
+    }
+
+    override fun data(): Data {
+        TODO("Not yet implemented")
+    }
+
+    override fun groups(): Groups {
+        TODO("Not yet implemented")
+    }
+
+    override fun profile(): Profile {
+        TODO("Not yet implemented")
+    }
+
+    override fun accounts() = accounts(false)
+
+    override fun accounts(isProfile: Boolean): Accounts {
+        TODO("Not yet implemented")
+    }
+
+    override val permissions: ContactsPermissions = contacts.permissions
+
+    override val applicationContext: Context = contacts.applicationContext
+
+    override val customDataRegistry: CustomDataRegistry = contacts.customDataRegistry
+}
