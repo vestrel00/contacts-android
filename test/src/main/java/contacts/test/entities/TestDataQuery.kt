8ede41b90cc2df06b023f1106a77a640eebd87ea
Vandolf Estrellado <vestrel00@gmail.com>
2021-11-30 18:23:33 -1000

    #113 Fixed compile error when clean and rebuilding project

--- test/src/main/java/contacts/test/entities/TestDataQuery.kt
+++
@@ -1,7 +0,0 @@
-package contacts.test.entities
-
-import contacts.core.data.DataQuery
-import contacts.core.data.DataQueryFactory
-
-internal fun DataQueryFactory.testData(): DataQuery<TestDataField, TestData> =
-    customData(TestDataMimeType)
