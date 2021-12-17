896ded7800a0d76794c3f448672c7d5a0ceadc61
Vandolf Estrellado <vestrel00@gmail.com>
2021-12-17 12:04:08 -1000

    Fixed compile error

--- test/src/main/java/contacts/test/entities/TestDataCursor.kt
+++ test/src/main/java/contacts/test/entities/TestDataCursor.kt
@@ -6,5 +6,5 @@ import contacts.core.entities.custom.AbstractCustomDataCursor
 internal class TestDataCursor(cursor: Cursor, includeFields: Set<TestDataField>) :
     AbstractCustomDataCursor<TestDataField>(cursor, includeFields) {
 
-    val value: String by nonNullString(TestDataFields.Value, TestDataEntity.VALUE)
+    val value: String by nonNullString(TestDataFields.Value, "test")
 }
--- test/src/main/java/contacts/test/entities/TestDataFieldMapper.kt
+++ test/src/main/java/contacts/test/entities/TestDataFieldMapper.kt
@@ -4,7 +4,7 @@ import contacts.core.entities.custom.CustomDataFieldMapper
 
 internal class TestDataFieldMapper : CustomDataFieldMapper<TestDataField, TestDataEntity> {
 
-    override fun valueOf(field: TestDataField, customDataEntity: TestDataEntity): String = when (field) {
+    override fun valueOf(field: TestDataField, customDataEntity: TestDataEntity): String? = when (field) {
         TestDataFields.Value -> customDataEntity.value
         else -> throw TestDataException("Unrecognized test data field $field")
     }
