637db3ca52ba87fde3263e7457f8cf2be48f0de2
Bernat Borrás <bernat.borras@adevinta.com>
2021-12-22 10:24:48 +0100

    Add AndroidLogger

--- core/src/main/java/contacts/core/log/Logger.kt
+++ core/src/main/java/contacts/core/log/Logger.kt
@@ -1,9 +1,21 @@
 package contacts.core.log
 
+import android.util.Log
+
 interface Logger {
   fun log(message: String)
 }
 
 class EmptyLogger : Logger {
   override fun log(message: String) {}
+}
+
+class AndroidLogger : Logger {
+  override fun log(message: String) {
+    Log.d(TAG, message)
+  }
+
+  companion object {
+    private const val TAG = "ContactsDebug"
+  }
 }
