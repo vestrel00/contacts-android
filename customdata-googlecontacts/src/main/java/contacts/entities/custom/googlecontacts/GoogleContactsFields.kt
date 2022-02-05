63d84b8d3f1acce39a68cc1f69170c3d1f173fee
Vandolf Estrellado <vestrel00@gmail.com>
2022-02-05 12:42:20 -1000

    #165 Added GoogleContactsFields

---
+++ customdata-googlecontacts/src/main/java/contacts/entities/custom/googlecontacts/GoogleContactsFields.kt
@@ -0,0 +1,59 @@
+package contacts.entities.custom.googlecontacts
+
+import contacts.core.AbstractCustomDataField
+import contacts.core.AbstractCustomDataField.ColumnName
+import contacts.core.AbstractCustomDataFieldSet
+import contacts.core.entities.MimeType
+
+class GoogleContactsFields : AbstractCustomDataFieldSet<GoogleContactsField>() {
+
+    @JvmField
+    val FileAs = FileAsFields()
+
+    @JvmField
+    val UserDefined = UserDefinedFields()
+
+    override val all: Set<GoogleContactsField> = FileAs.all + UserDefined.all
+
+    override val forMatching: Set<GoogleContactsField> =
+        FileAs.forMatching + UserDefined.forMatching
+}
+
+sealed class GoogleContactsField(columnName: ColumnName) : AbstractCustomDataField(columnName)
+
+data class FileAsField internal constructor(private val columnName: ColumnName) :
+    GoogleContactsField(columnName) {
+
+    override val customMimeType: MimeType.Custom = GoogleContactsMimeType.FileAs
+}
+
+class FileAsFields internal constructor() : AbstractCustomDataFieldSet<FileAsField>() {
+
+    @JvmField
+    val Name = FileAsField(ColumnName.DATA)
+
+    override val all: Set<FileAsField> = setOf(Name)
+
+    override val forMatching: Set<FileAsField> = setOf(Name)
+}
+
+data class UserDefinedField internal constructor(private val columnName: ColumnName) :
+    GoogleContactsField(columnName) {
+
+    override val customMimeType: MimeType.Custom = GoogleContactsMimeType.UserDefinedField
+}
+
+class UserDefinedFields internal constructor() : AbstractCustomDataFieldSet<UserDefinedField>() {
+
+    // Google chose to use the "TYPE" (data2) for the custom field name.
+    @JvmField
+    val Field = UserDefinedField(ColumnName.TYPE)
+
+    // Google chose to use the data1 for the custom field value.
+    @JvmField
+    val Label = UserDefinedField(ColumnName.DATA)
+
+    override val all: Set<UserDefinedField> = setOf(Field, Label)
+
+    override val forMatching: Set<UserDefinedField> = setOf(Field, Label)
+}
