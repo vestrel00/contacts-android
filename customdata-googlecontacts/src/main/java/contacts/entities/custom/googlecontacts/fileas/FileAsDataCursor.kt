26ea0c33c7741c0c01da6e0f724af779e5c09cd1
Vandolf Estrellado <vestrel00@gmail.com>
2022-02-05 14:19:04 -1000

    #165 Implemented mappers

--- customdata-googlecontacts/src/main/java/contacts/entities/custom/googlecontacts/fileas/FileAsDataCursor.kt
+++ customdata-googlecontacts/src/main/java/contacts/entities/custom/googlecontacts/fileas/FileAsDataCursor.kt
@@ -2,8 +2,8 @@ package contacts.entities.custom.googlecontacts.fileas
 
 import android.database.Cursor
 import contacts.core.entities.custom.AbstractCustomDataCursor
-import contacts.entities.custom.googlecontacts.GoogleContactsFields
 import contacts.entities.custom.googlecontacts.FileAsField
+import contacts.entities.custom.googlecontacts.GoogleContactsFields
 
 internal class FileAsDataCursor(cursor: Cursor, includeFields: Set<FileAsField>) :
     AbstractCustomDataCursor<FileAsField>(cursor, includeFields) {
---
+++ customdata-googlecontacts/src/main/java/contacts/entities/custom/googlecontacts/userdefined/UserDefinedMapper.kt
@@ -0,0 +1,32 @@
+package contacts.entities.custom.googlecontacts.userdefined
+
+import android.database.Cursor
+import contacts.core.entities.custom.AbstractCustomDataEntityMapper
+import contacts.entities.custom.googlecontacts.UserDefinedField
+
+internal class UserDefinedMapperFactory :
+    AbstractCustomDataEntityMapper.Factory<UserDefinedField, UserDefinedDataCursor, UserDefined> {
+
+    override fun create(
+        cursor: Cursor, includeFields: Set<UserDefinedField>
+    ): AbstractCustomDataEntityMapper<UserDefinedField, UserDefinedDataCursor, UserDefined> =
+        UserDefinedMapper(UserDefinedDataCursor(cursor, includeFields))
+}
+
+private class UserDefinedMapper(cursor: UserDefinedDataCursor) :
+    AbstractCustomDataEntityMapper<UserDefinedField, UserDefinedDataCursor, UserDefined>(cursor) {
+
+    override fun value(cursor: UserDefinedDataCursor) = UserDefined(
+        id = cursor.dataId,
+        rawContactId = cursor.rawContactId,
+        contactId = cursor.contactId,
+
+        isPrimary = cursor.isPrimary,
+        isSuperPrimary = cursor.isSuperPrimary,
+
+        field = cursor.field,
+        label = cursor.label,
+
+        isRedacted = false
+    )
+}
