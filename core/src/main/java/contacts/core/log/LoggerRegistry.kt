76b29fe5d944bdc766c7d01870906bb781bdf739
Vandolf Estrellado <vestrel00@gmail.com>
2022-06-10 12:51:06 -1000

    Fixed execution time math (#214)

--- core/src/main/java/contacts/core/log/LoggerRegistry.kt
+++ core/src/main/java/contacts/core/log/LoggerRegistry.kt
@@ -10,7 +10,10 @@ class LoggerRegistry(logger: Logger) {
 
     // Prevent consumers from invoking the listener functions by not having the registry implement
     // it directly.
-    private class Listener(private val logger: Logger, private val apiExecutionStartTimeMillis: MutableMap<CrudApi, Long> = mutableMapOf()) : CrudApi.Listener {
+    private class Listener(
+        private val logger: Logger,
+        private val apiExecutionStartTimeMillis: MutableMap<CrudApi, Long> = mutableMapOf()
+    ) : CrudApi.Listener {
 
         override fun onPreExecute(api: CrudApi) {
             apiExecutionStartTimeMillis[api] = System.currentTimeMillis()
@@ -18,12 +21,13 @@ class LoggerRegistry(logger: Logger) {
         }
 
         override fun onPostExecute(api: CrudApi, result: CrudApi.Result) {
-            val executionTime = apiExecutionStartTimeMillis.remove(api)
+            val execTimeMillis = apiExecutionStartTimeMillis.remove(api)?.let { startTimeMillis ->
+                System.currentTimeMillis() - startTimeMillis
+            }
 
             logger.log(
                 """
-                   ${api.redactedCopyOrThis(logger.redactMessages)}
-                   execution time: $executionTime
+                   ${api.redactedCopyOrThis(logger.redactMessages)} ($execTimeMillis milliseconds)
                 """.trimIndent()
             )
         }
